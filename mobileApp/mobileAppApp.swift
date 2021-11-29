7338033c1795bcf2d4c19331e8569b374273ae88
kimulia1 <ekzit96@gmail.ru>
2021-11-30 00:37:13 +0700

    Initial Commit

---
+++ mobileApp/mobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileAppApp.swift
+//  mobileApp
+//
+//  Created by Максим on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct mobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
