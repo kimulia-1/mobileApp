5fc83af15041862d61e029c42b20280df41e35c9
kimulia1 <ekzit96@gmail.ru>
2021-11-30 00:41:12 +0700

    hello git

--- mobileApp/ContentView.swift
+++ mobileApp/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, git!")
             .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
